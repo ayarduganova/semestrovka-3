095adee2732765ed92f12bd8a371cdfca0d59d97
Anastasiya Arduganova <AYArduganova@stud.kpfu.ru>
2024-05-27 23:39:20 +0300

    add ajax, rest, page with map

--- spring-boot-semestrovka/src/main/resources/static/js/map.js
+++ spring-boot-semestrovka/src/main/resources/static/js/map.js
@@ -12,51 +12,19 @@ function init() {
         addPlacemark(lat, lon);
     }
 
-    // Обработчик событий для приближения карты к метке
     addressList.addEventListener('click', function(event) {
         var target = event.target;
         if (target.tagName === 'LI') {
             var lat = parseFloat(target.getAttribute('data-lat'));
             var lon = parseFloat(target.getAttribute('data-lon'));
             myMap.setCenter([lat, lon]);
-            myMap.setZoom(15); // Установите желаемый уровень зума
+            myMap.setZoom(15);
         }
     });
 
     function addPlacemark(lat, lon) {
-        // var coords = [lat, lon];
-        // var myPlacemark = new ymaps.Placemark(coords, {
-        //     hintContent: 'Здесь может быть ваше название'
-        // });
-        // myMap.geoObjects.add(myPlacemark);
-
         var coords = [lat, lon];
-        var myPlacemark = new ymaps.Placemark(coords, {
-            hintContent: 'Здесь может быть ваше название'
-        });
-
-        // Добавляем обработчик события добавления метки на карту
-        myMap.events.add('add', function (event) {
-            // Получаем объект метки, который был добавлен
-            var placemark = event.get('target');
-
-            // Получаем координаты метки
-            var lat = placemark.geometry.getCoordinates()[0];
-            var lon = placemark.geometry.getCoordinates()[1];
-
-            // Выполняем обратное геокодирование для получения адреса
-            ymaps.geocode([lat, lon]).then(function (res) {
-                var firstGeoObject = res.geoObjects.get(0);
-                var address = firstGeoObject.getAddressLine();
-
-                // Находим элемент списка, соответствующий данной метке
-                var li = document.querySelector('li[data-lat="' + lat + '"][data-lon="' + lon + '"]');
-
-                // Меняем текст элемента списка на адрес
-                li.textContent = address;
-            });
-        });
-
+        var myPlacemark = new ymaps.Placemark(coords);
         myMap.geoObjects.add(myPlacemark);
     }
 
